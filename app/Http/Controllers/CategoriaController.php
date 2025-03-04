9c0ccc76eeecd2d50865224f89648239d78f5875
Braian <bf02rojas.edu@gmail.com>
2025-03-03 22:42:04 -0300

    Respuesta JSON para las categorias con API Resource

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaCollection;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        //dd('desde API/categoriaController');
+        //return response()->json(['categorias' => Categoria::all()]);
+
+        return new CategoriaCollection(Categoria::all());
+    }
 }
