1956a275a4319126b4bd92f21174499034d48945
Braian <bf02rojas.edu@gmail.com>
2025-03-04 23:04:09 -0300

    Filtrar productos disponibles y ordenarlos por ID descendente

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -16,9 +16,9 @@ class ProductoController extends Controller
     public function index()
     {
         //
-        //return new ProductoCollection(Producto::where('disponible', 1)->orderBy('id', 'DESC')->paginate(10));
- 
-        return new ProductoCollection(Producto::all());
+        
+        //return new ProductoCollection(Producto::all());
+        return new ProductoCollection(Producto::where('disponible', 1)->orderBy('id', 'DESC')->get());
     }
 
     /**
