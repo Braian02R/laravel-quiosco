4ea1056c16977e3488c93704c909efc40892fd04
Braian <bf02rojas.edu@gmail.com>
2025-03-10 05:33:21 -0300

    endpoint de logout + revocar token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,6 +56,12 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
+        //return "logout...";
+        $user = $request->user();
+        $user->currentAccesstoken()->delete();
 
+        return [
+            'user' => null
+        ];
     }
 }
